f7d72e84c875c3e06f8937f8569be6eecb35be33
Noted <kwangedwin@gmail.com>
2025-08-10 18:40:18 +1000

    Created a basic UserService to access user data

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject, signal, type OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +13,18 @@ export class HomeComponent implements OnInit {
 
   username = signal(sessionStorage.getItem('username') || '');
 
+  userService = inject(UserService);
+
   router = inject(Router);
 
   ngOnInit(): void { 
     if (!sessionStorage.getItem('token')) {
       this.router.navigate(['/login']);
     }
+  }
+
+  getUsers() {
+    this.userService.getUsers().subscribe((users) => console.log(users));
   }
 
 }
