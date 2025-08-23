03e37b21d538f836b48a6b1b8e0ff599314121d5
Noted <kwangedwin@gmail.com>
2025-08-23 12:11:51 +1000

    completed File Dropping Feature & Initialized Project Dialogue Component

---
+++ client/src/app/project-dialogue/project-dialogue.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-dialogue',
+  imports: [],
+  templateUrl: './project-dialogue.component.html',
+  styleUrl: './project-dialogue.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProjectDialogueComponent { }
