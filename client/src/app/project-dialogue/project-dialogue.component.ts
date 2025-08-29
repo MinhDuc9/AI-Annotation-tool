72f125185c4b08bf1c2369c44d971f130df9822a
Noted <kwangedwin@gmail.com>
2025-08-29 19:41:17 +1000

    Finished Dialogue Contents

--- client/src/app/project-dialogue/project-dialogue.component.ts
+++ client/src/app/project-dialogue/project-dialogue.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
-import { FormBuilder, FormControl, FormGroupDirective, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject, input, model, signal } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroupDirective, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,7 +7,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FileInputComponent } from '../components/file-input/file-input.component';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { MatStepper, MatStepperModule } from '@angular/material/stepper';
+import { MatSelectModule } from '@angular/material/select';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
+interface Role {
+    roleName: string;
+    value: number;
+}
 
 export class ProjectCreationErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -16,58 +23,106 @@ export class ProjectCreationErrorStateMatcher implements ErrorStateMatcher {
   }
 }
 @Component({
-  selector: 'app-project-dialogue',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, MatIconModule, FileInputComponent],
-  templateUrl: './project-dialogue.component.html',
-  styleUrl: './project-dialogue.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-project-dialogue',
+    imports: [
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        FileInputComponent,
+        MatStepperModule,
+        MatSelectModule,
+        MatDialogModule
+    ],
+    templateUrl: './project-dialogue.component.html',
+    styleUrl: './project-dialogue.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectDialogueComponent {
-  private _snackBar = inject(MatSnackBar);
-  private _formBuilder = inject(FormBuilder);
+    private _snackBar = inject(MatSnackBar);
+    private _formBuilder = inject(FormBuilder);
+    readonly dialogRef = inject(MatDialogRef<ProjectDialogueComponent>);
+    readonly data = inject<String>(MAT_DIALOG_DATA);
+    readonly user_token = model(this.data);
 
-  matcher = new ProjectCreationErrorStateMatcher();
-
-
-  access = ['read-only', 'admin'];
-  username = input('');
-  files: File[] = [];
-
-  projectForm = this._formBuilder.group({
-    projectName: ['', Validators.required],
-    authorization: this._formBuilder.group({
-      emails: this._formBuilder.array([this._formBuilder.control('', Validators.email)]),
-      roles: this._formBuilder.array([this._formBuilder.control('')]),
-    }),
-  });
-
-  removeFile(index: number) {
-    if (this.files.length > 0){
-      this.files.splice(index, 1);
+    onNoClick(): void {
+      this.dialogRef.close();
     }
-  }
 
-  uploadFile(files: FileList) {
-    this.files.push(...Array.from(files));
-  }
 
-  projectName(){
-    return this.projectForm.get('projectName');
-  }
+    matcher = new ProjectCreationErrorStateMatcher();
 
-  emails(){
-    return this.projectForm.get('authorization.emails');
-  }
+    roleChoices: Role[] = [
+        { roleName: 'Write', value: 1 },
+        { roleName: 'Read', value: 2 },
+    ];
 
-  roles(){
-    return this.projectForm.get('authorization.roles');
-  }
+    username = input('');
+    files: File[] = [];
+    fileError = signal(false);
+    nameEmpty = signal(false);
 
-  onSubmit() {
-    if (this.projectForm.valid) {
-      this._snackBar.open('Project created successfully', 'Close', {
-        duration: 3000,
-      });
+    projectForm = this._formBuilder.group({
+        projectName: ['', Validators.required],
+        authorization: this._formBuilder.group({
+            emails: this._formBuilder.array([
+                this._formBuilder.control('', Validators.email),
+            ]),
+            roles: this._formBuilder.array([this._formBuilder.control(2)]),
+        }),
+    });
+
+    removeFile(index: number) {
+        if (this.files.length > 0) {
+            this.files.splice(index, 1);
+        }
     }
-  }
+
+    uploadFile(files: FileList) {
+        this.files.push(...Array.from(files));
+    }
+
+    projectName() {
+        return this.projectForm.get('projectName');
+    }
+
+    emails() {
+        return this.projectForm.get('authorization.emails') as FormArray<
+            FormControl<string | null>
+        >;
+    }
+
+    roles() {
+        return this.projectForm.get('authorization.roles') as FormArray<
+            FormControl<string | null>
+        >;
+    }
+
+    addEntry() {
+        this.emails().push(this._formBuilder.control('', Validators.email));
+        this.roles().push(this._formBuilder.control(''));
+    }
+
+    goStep2(stepper: MatStepper) {
+        this.projectName()?.markAsTouched();
+        if (this.projectName()?.valid) {
+            stepper.next();
+        }
+    }
+
+    goStep3(stepper: MatStepper) {
+        this.emails().markAllAsTouched();
+        if (this.emails().valid) {
+            stepper.next();
+        }
+    }
+
+    onSubmit() {
+        if (this.projectForm.valid) {
+            this._snackBar.open('Project created successfully', 'Close', {
+                duration: 3000,
+            });
+        }
+    }
 }
